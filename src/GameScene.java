4345c633932364503d71f8d1e268edb3fc6d713d
despance <despance1927@hotmail.com>
2022-04-24 01:21:23 +0300

    ilk sahne

--- src/GameScene.java
+++ src/GameScene.java
@@ -1,6 +1,10 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class GameScene extends Application {
@@ -12,8 +16,32 @@ public class GameScene extends Application {
 
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage){
+        primaryStage.setTitle("Ball Pipe Maze");
+
+
         BorderPane borderPane = new BorderPane();
+        Pane pane = new Pane();
+
+
+
+        //pane.getChildren().add(new Button("click me"));
+
+        borderPane.setCenter(pane);
+
+
+        borderPane.setBottom(new Button("click me"));
+
+        borderPane.setLeft(new Button("click me"));
+        ((Button) borderPane.getLeft()).setAlignment(Pos.CENTER);
+        borderPane.setRight(new Button("click me"));
+        ((Button) borderPane.getRight()).setAlignment(Pos.CENTER);
+        borderPane.setTop(new Button("click me"));
+        ((Button) borderPane.getTop()).setAlignment(Pos.CENTER);
+
+
+
+
         Scene scene = new Scene(borderPane,300 ,300);
         primaryStage.setScene(scene);
         primaryStage.show();
